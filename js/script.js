ab1e81be2d6c861cfe9645f77fdd2f785d6dee2c
Elleonors <Wauquier.Teddy@gmail.com>
2019-02-07 14:55:16 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 var score =0;
+var cookieperclick = 1;
+
+(function() {
+
+function addcookie(score){
+    score = score + cookieperclick;
+    displayscore(score);
+}
+    setInterval(function(){ 
+        displayscore(score); 
+        console.log( "hello" )
+    }, 10);
+    
+    function displayscore(){
+        document.getElementsByClassName("affichage")[0].setAttribute("value", score);
+        document.getElementsByClassName("affichage")[0].innerHTML = "here your cookies : " + score;
+    };
+    
+    document.getElementById("click").addEventListener("click", function(){
+        addcookie(score);
+    });
+
+
+}());
+
+
+
+
+
