2736a86ebc28d9198e1b1f7605a791c2e5dda9f4
user <broutin.jmarc@gmail.com>
2019-02-07 14:47:59 +0100

    multiplier + price

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
-var score =0;
+var score = 0;
+var cookieperclick = 1;
+var multiplicateur = 1;
+var mult_price = 10;
+
+(function (){
+
+    function addcookie(){
+        score = score + cookieperclick;
+
+    };
+
+    function augmenterMultiplicateur (){
+        multiplicateur = multiplicateur + 1;
+    };
+
+    function make_price(price){
+        return price*2;
+    };
+    
+    document.getElementById("click").addEventListener("click", function(){
+        addcookie(score);
+        console.log(score);
+    })
+    
+    
+    
+        document.getElementById("multiplier").addEventListener("click", function(){
+        
+            if (score >= mult_price){
+                augmenterMultiplicateur(multiplicateur);
+                document.getElementById("multiplier").innerHTML = "Multiplicateur x"+ (multiplicateur+1);
+                cookieperclick = cookieperclick * multiplicateur;
+                score = score - mult_price;
+                mult_price = make_price(mult_price);
+                console.log(multiplicateur, cookieperclick, score, mult_price);
+            };
+    })
+
+}());
+
+
+
+
