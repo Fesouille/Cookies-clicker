da9933a225eb657cd16bc6f9e021731470e2874c
Fesouille <ismail.jsph@gmail.com>
2019-02-08 00:18:26 +0100

    autoclick final

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 var score =0;
 var cookieperclick = 1;
+//autoclick variables - BEGIN
 var autoclick=false;
-var free_autoclick;
+var autoclick_button=document.createElement("button");
+autoclick_button.setAttribute("type", "button");
+autoclick_button.setAttribute("id", "autoclick");
+var autoclick_timer;
+var autoclick_price=5;
+//Autoclick variables - END
 
 function addcookie(){
     score = score + cookieperclick;
     displayscore(score);
-    console.log(score);
     add_autoclick();
 }
 
@@ -18,27 +23,47 @@ function displayscore(){
 function make_price(price){
 	return price*2;
 }
-//!! this must be placed everywhere the score is updated !!
+
+//Autoclick functions - BEGIN
+/*this function checks if user got enough cookies to buy an autoclick*/
+function check_autoclick(){
+	if(score<autoclick_price){
+			autoclick_button.disabled=true;
+		}
+		if(score>=autoclick_price){
+			autoclick_button.disabled=false;
+		}
+}
+
+/*Initialize the autoclick
+i.e. if it does not exists, it creates it. If it does, it calls the check_autoclick() functions
+WARNING: This functions must be added in the addcookie() function or any function that add cookies to the counter*/
 function add_autoclick(){
 	if(autoclick==false){
-		if(score==10){
-			var autoclick_button=document.createElement("button");
-			autoclick_button.setAttribute("type", "button");
-			autoclick_button.setAttribute("id", "autoclick");
-			autoclick_button.setAttribute("style","opacity=0.5")
-			document.querySelector(".maincontainer").appendChild(autoclick_button).innerHTML="Auto-click";
+		if(score==autoclick_price){
+			document.querySelector(".maincontainer").appendChild(autoclick_button).innerHTML="Auto-click,  price="+autoclick_price;
 			autoclick=true;
 		}
 	}
 	else{
-		if(score>=10 && score <20){
-			document.selectElementById("autoclick").style.opacity=0.5;
-		}
-		if(score>=20){
-			document.selectElementById("autoclick").style.opacity=0;
-		}
+		check_autoclick();
 	}
 }
+/*To buy an autoclick
+- on click, the score is decremented by the autoclick_price
+- the score on screen is updated
+- a new autoclick_price is calculated and updated on screen
+- check_autoclick() function is called to make sure user cannot buy another one after new price is calculated
+- finally, launch an interval for the function addcookie() every seconds*/
+autoclick_button.addEventListener("click", function(){
+	score-=autoclick_price;
+	displayscore(score);
+	autoclick_price=make_price(autoclick_price);
+	autoclick_button.innerHTML="Auto-click, price="+autoclick_price;
+	check_autoclick();
+	setInterval(addcookie, 1000);
+})
+//Autoclick functions - END
 
 document.getElementById("click").addEventListener("click", function(){
     addcookie();
